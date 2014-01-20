ab2ec70c55554253beb4632ebad5b21f708b2f9e
SayCV <sssxyc@163.com>
2014-01-20 17:30:17 +0800

    ...

--- include/sigar.h
+++ include/sigar.h
@@ -119,6 +119,27 @@ typedef unsigned long sigar_gid_t;
 typedef pid_t sigar_pid_t;
 typedef uid_t sigar_uid_t;
 typedef gid_t sigar_gid_t;
+
+#if defined(__android__)
+
+#define JNI_DEBUG  
+  
+#ifdef JNI_DEBUG  
+  
+#ifndef LOG_TAG  
+#define LOG_TAG "JNI_DEBUG"  
+#endif  
+  
+#include <android/log.h>  
+  
+#define LOGE(msg) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, msg)  
+#define LOGI(msg) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, msg)  
+#define LOGD(msg) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, msg)  
+  
+#endif
+
+#endif
+
 #endif
 
 typedef struct sigar_t sigar_t;
