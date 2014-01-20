484a823837987253556e4ba3b5e4eed7875892b5
SayCV <sssxyc@163.com>
2014-01-21 00:03:45 +0800

    ...

--- include/sigar.h
+++ include/sigar.h
@@ -132,14 +132,14 @@ typedef gid_t sigar_gid_t;
 #ifdef JNI_DEBUG
 
 #ifndef LOG_TAG
-#define LOG_TAG "JNI_DEBUG"
+#define LOG_TAG "SIGAR_DEBUG"
 #endif
 
 #include <android/log.h>
 
-#define LOGE(msg) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "__file__ -> " msg)
-#define LOGI(msg) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "__file__ -> " msg)
-#define LOGD(msg) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "__file__ -> " msg)
+#define LOGE(msg) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, msg)
+#define LOGI(msg) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, msg)
+#define LOGD(msg) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, msg)
 	
 #endif
 
--- include/trace.h
+++ include/trace.h
@@ -13,19 +13,22 @@
 
 #if (DEBUG_FLAG == 1)
 
+#include <android/log.h>
+#define TRACE_TAG "SIGAR_TRACE"
+
 static unsigned int __sdebug_time = 0;
 #define  __pdebug_Msg_Size 1024
 static char __pdebug_Msg[__pdebug_Msg_Size];
 //注意下面的__attribute__不是C标准。。。。
 static void __debug_info(const char *prefix,const char *fmt, ...) __attribute__((format (printf, 2, 3)));
 #define __NEXT_DBG_TIME() do{sdebug_time++;}while(0)
-#define __PRINT_POS() do {fprintf(stderr,"%s(%d){%s}",__FILE__,__LINE__,__func__);}while(0)
-#define __FUNC_LOG() do{__PRINT_POS();fprintf(stderr,": t = %d ",__sdebug_time++);} while(0)
-#define __FUNC_LOGn() do{__FUNC_LOG();fprintf(stderr,"\n");} while(0)
-#define __PRINT_POS_exp(exp) do{__PRINT_POS();fprintf(stderr,"| (%s):",#exp);}while(0)
-#define __PRINT_POS_P_exp(prefix,exp) do{__PRINT_POS();fprintf(stderr,"|<%s> (%s):",prefix,#exp);}while(0)
-#define __PRINT_POS_expn(exp)  do{__PRINT_POS_exp(exp);fprintf(stderr,"\n");}while(0)
-#define __PRINT_POS_P_expn(prefix,exp) do{__PRINT_POS_P_exp(prefix,exp);fprintf(stderr,"\n");}while(0)
+#define __PRINT_POS() do {__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, "%s(%d){%s}",__FILE__,__LINE__,__func__);}while(0)
+#define __FUNC_LOG() do{__PRINT_POS();__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, ": t = %d ",__sdebug_time++);} while(0)
+#define __FUNC_LOGn() do{__FUNC_LOG();__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, "\n");} while(0)
+#define __PRINT_POS_exp(exp) do{__PRINT_POS();__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, "| (%s):",#exp);}while(0)
+#define __PRINT_POS_P_exp(prefix,exp) do{__PRINT_POS();__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, "|<%s> (%s):",prefix,#exp);}while(0)
+#define __PRINT_POS_expn(exp)  do{__PRINT_POS_exp(exp);__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, "\n");}while(0)
+#define __PRINT_POS_P_expn(prefix,exp) do{__PRINT_POS_P_exp(prefix,exp);__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, "\n");}while(0)
 #define __ASSERT(exp) do{if (exp){}else{__PRINT_POS_P_expn("ASSERT ERROR",exp);}}while (0)
 #define __ASSERT_EXIT(exp) do{if (exp){}else{__PRINT_POS_P_expn("ASSERT ERROR",exp);exit(1);}}while (0)
 
@@ -33,19 +36,21 @@ static void __debug_info(const char *prefix,const char *fmt, ...) __attribute__(
 
 #define __ASSERT_LOG(exp,fmt,...) __debug_info_LOG(exp,"ASSERT!",fmt,__VA_ARGS__)
 #define __ERROR_LOG(exp,fmt,...) __debug_info_LOG(exp,"ERROR!",fmt,__VA_ARGS__)
-#define __BEFORE_LOG(N,fmt,...)  do {__debug_info_LOG((N) < __sdebug_time,"BEFORE!",fmt,__VA_ARGS__);__NEXT_DBG_TIME()}while(0) 
-#define __AFTER_LOG(N,fmt,...) do {__debug_info_LOG((N) >= __sdebug_time,"AFTER!",fmt,__VA_ARGS__); __NEXT_DBG_TIME();}while(0) 
+#define __BEFORE_LOG(N,fmt,...)  do {__debug_info_LOG(N) < __sdebug_time,"BEFORE!",fmt,__VA_ARGS__);__NEXT_DBG_TIME()}while(0) 
+#define __AFTER_LOG(N,fmt,...) do {__debug_info_LOG(N) >= __sdebug_time,"AFTER!",fmt,__VA_ARGS__); __NEXT_DBG_TIME();}while(0) 
 static void __debug_info(const char *prefix,const char *fmt, ...) {
-	va_list params;	
+	va_list params;
 	va_start(params, fmt);
-	__ASSERT_EXIT((__pdebug_Msg) && (__pdebug_Msg_Size ));
+	__ASSERT_EXIT((__pdebug_Msg) && (__pdebug_Msg_Size));
 	vsnprintf(__pdebug_Msg, __pdebug_Msg_Size, fmt, params);
-	if (prefix){
-		fprintf(stderr, " %s %s\n", prefix, __pdebug_Msg);
-	}else{
-		fprintf(stderr, " %s\n", __pdebug_Msg);
-	}
 	va_end(params);
+	if (prefix){
+		//fprintf(stderr, " %s %s\n", prefix, __pdebug_Msg);
+		__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, " %s %s\n", prefix, __pdebug_Msg);
+	}else{
+		//fprintf(stderr, " %s\n", __pdebug_Msg);
+		__android_log_print(ANDROID_LOG_INFO, TRACE_TAG, " %s\n", __pdebug_Msg);
+	}
 }
 #else
 #define __NOP do{}while(0)
